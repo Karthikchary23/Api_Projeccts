d53dbc24fae78263d32925a51e798a0498b2d96b
Karthikcahry23 <lingojikarthikchary@gmail.com>
2024-04-19 16:15:42 +0530

    success

--- Api project 1/index.js
+++ Api project 1/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const database = require("./dataBase.js");
 const booky = express();
 
+booky.use(bodyParser.urlencoded({ extended: true }));
+booky.use(bodyParser.json());
+
 // Route to get all books
-booky.get("/books", (req, res) => {
+booky.get("/", (req, res) => {
     return res.json({ books: database.books });
 });
 
@@ -40,7 +44,7 @@ booky.get("/author/:authorId", (req, res) => {
 // Route to get books by author name
 booky.get("/books/author/:authorName", (req, res) => {
     const authorName = req.params.authorName;
-    const booksByAuthor = database.books.filter((books) => books.authorName.includes(authorName));
+    const booksByAuthor = database.books.filter(book => book.authorName.includes(authorName));
     if (booksByAuthor.length === 0) {
         return res.json({ error: `No books found by author ${authorName}` });
     }
@@ -48,12 +52,12 @@ booky.get("/books/author/:authorName", (req, res) => {
 });
 
 // Route to get all authors
-booky.get("/authors", (req, res) => {
+booky.get("/author", (req, res) => {
     return res.json({ authors: database.author });
 });
 
 // Route to get author details by author name
-booky.get("/authors/:authorName", (req, res) => {
+booky.get("/author/:authorName", (req, res) => {
     const authorName = req.params.authorName;
     const author = database.author.find(author => author.authorName === authorName);
     if (!author) {
@@ -61,36 +65,49 @@ booky.get("/authors/:authorName", (req, res) => {
     }
     return res.json({ author });
 });
-//author details based on isbn
 
-booky.get("/author/book/:isbn",(req,res)=>
-{
-    const getAuthorDetails=database.author.filter((author)=>author.books.includes(req.params.isbn));
-    if(getAuthorDetails.length===0)
-    {
-        return res.json({error:`no author details found based on ${res.params.isbn} ID`});
+// Route to get author details based on ISBN
+booky.get("/author/book/:isbn", (req, res) => {
+    const getAuthorDetails = database.author.filter(author => author.books.includes(req.params.isbn));
+    if (getAuthorDetails.length === 0) {
+        return res.json({ error: `No author details found based on ${req.params.isbn} ID` });
     }
-    return res.json({getAuthorDetails})
-
+    return res.json({ author: getAuthorDetails });
 });
-//publications
 
-booky.get("/publication",(req,res)=>
-{
-    return res.json({publications:database.publication})
-})
-//publication Id
-booky.get("/publication/:id",(req,res)=>
-{
-    const getPublicationDetails=database.publication.filter((publication)=>publication.id ===parseInt(req.params.id));
-    if(getPublicationDetails.length===0)
-    {
+// Route to get all publications
+booky.get("/publication", (req, res) => {
+    return res.json({ publications: database.publication });
+});
+
+// Route to get publication details by ID
+booky.get("/publication/:id", (req, res) => {
+    const publicationId = parseInt(req.params.id);
+    const getPublicationDetails = database.publication.filter(publication => publication.id === publicationId);
+    if (getPublicationDetails.length === 0) {
         return res.json({ error: `No publication details found based on ${req.params.id}` });
     }
     return res.json({ publication: getPublicationDetails[0] });
 });
 
-
+// Route to add a new book
+booky.post("/book/new", (req, res) => {
+    const newBook = req.body;
+    database.books.push(newBook);
+    return res.json({ updatedBooks: database.books });
+});
+//Route to add new author
+booky.post("/author/new", (req, res) => {
+    const newAuthor = req.body;
+    database.author.push(newAuthor);
+    return res.json({ updatedBooks: database.author });
+});
+//Route to add new publication
+booky.post("/publication/new", (req, res) => {
+    const newPublication = req.body;
+    database.publication.push(newPublication);
+    return res.json({ updatedBooks: database.publication });
+});
 
 booky.listen(3000, () => {
     console.log("Server running on port 3000");
